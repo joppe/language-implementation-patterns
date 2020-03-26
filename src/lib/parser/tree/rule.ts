2917c090b145ec8574c20f35421b0aef5afa6185
Joppe Aarts <joppe@apestaartje.info>
2020-03-26 13:39:10 +0100

    cleanup

--- src/lib/parser/tree/rule.ts
+++ src/lib/parser/tree/rule.ts
@@ -5,13 +5,6 @@ type RuleMethod = () => void;
 export function rule(target: Parser, key: string, descriptor: TypedPropertyDescriptor<RuleMethod>): TypedPropertyDescriptor<RuleMethod> {
     const original: () => void = descriptor.value ?? (target[key]);
 
-    // descriptor.value = (): void => {
-    //     console.log(this);
-    //     original.call(target);
-    // };
-
-    // return descriptor;
-
     return {
         get(): RuleMethod {
             return (): void => {
