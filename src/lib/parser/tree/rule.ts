7df3061baab986bb01bfc282a9eaaf91663277ff
Joppe Aarts <joppe@apestaartje.info>
2020-03-26 13:45:48 +0100

    tslint fix

--- src/lib/parser/tree/rule.ts
+++ src/lib/parser/tree/rule.ts
@@ -3,12 +3,12 @@ import { Parser } from '../Parser';
 type RuleMethod = () => void;
 
 export function rule(target: Parser, key: string, descriptor: TypedPropertyDescriptor<RuleMethod>): TypedPropertyDescriptor<RuleMethod> {
-    const original: () => void = descriptor.value ?? (target[key]);
+    const original: RuleMethod = <RuleMethod>descriptor.value ?? <RuleMethod>(target[key]);
 
     return {
         get(): RuleMethod {
             return (): void => {
-                // tslint:disable-next-line no-invalid-this
+                // tslint:disable-next-line no-invalid-this no-unsafe-any
                 this.rule(key, original.bind(this));
             };
         },
